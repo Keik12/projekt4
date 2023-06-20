b56517f6a1e7d7e349e38b3e82c3b80619d94d61
Emil Kubela <emilkubela4@gmail.com>
2023-06-20 21:23:38 +0200

    Initial Commit

--- index.js
+++ index.js
@@ -2,19 +2,35 @@ const amountValue = document.getElementById("amountValue");
 const chooseCurrency = document.getElementById("chooseCurrency");
 const countResultBtn = document.getElementById("countResultBtn");
 const amountConverted = document.getElementById("amountConverted");
+const wrongValue = document.getElementById("wrongValue");
+const wrongCurrency = document.getElementById("wrongCurrency");
 
 const getCurrency = () => {
-  if (chooseCurrency.value && amountValue.value) {
-    fetch(`http://api.nbp.pl/api/exchangerates/rates/A/${chooseCurrency.value}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const rates = data.rates[0].mid;
-        currencyExchange(rates);
-      })
-      .catch((err) => console.log(err));
-  } else {
-    alert("Wybierz walutę");
+  if (!chooseCurrency.value) {
+    document.getElementById("wrongCurrency").style.color = "red";
+    return (wrongCurrency.innerHTML = "Wybierz walutę");
   }
+  if (!amountValue.value) {
+    document.getElementById("wrongValue").style.color = "red";
+    return (wrongValue.innerHTML = "Wprowadź kwotę");
+  }
+  if (amountValue.value <= 0) {
+    document.getElementById("wrongValue").style.color = "red";
+    return (wrongValue.innerHTML = "Wartośc musi być większa niż zero");
+  }
+
+  wrongValue.innerHTML = "";
+  wrongCurrency.innerHTML = "";
+
+  fetch(`http://api.nbp.pl/api/exchangerates/rates/A/${chooseCurrency.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const rates = data.rates?.[0]?.mid;
+      if (rates) {
+        currencyExchange(rates);
+      }
+    })
+    .catch((err) => console.log(err));
 };
 
 function currencyExchange(currency) {
