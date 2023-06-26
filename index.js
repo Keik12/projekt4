94b576e159efb3c6b756fb77a2ec6fccddd8ad0d
Emil Kubela <emilkubela4@gmail.com>
2023-06-26 09:49:11 +0200

    Initial Commit

--- index.js
+++ index.js
@@ -3,26 +3,22 @@ const chooseCurrency = document.getElementById("chooseCurrency");
 const countResultBtn = document.getElementById("countResultBtn");
 const amountConverted = document.getElementById("amountConverted");
 const wrongValue = document.getElementById("wrongValue");
-const wrongCurrency = document.getElementById("wrongCurrency");
 
 const getCurrency = () => {
-  if (!chooseCurrency.value) {
-    document.getElementById("wrongCurrency");
-    return (wrongCurrency.innerHTML = "Wybierz walutę");
-  }
   if (!amountValue.value) {
     document.getElementById("wrongValue");
-    return (wrongValue.innerHTML = "Wprowadź kwotę");
+    wrongValue.textContent = "Wprowadź kwotę";
+    return;
   }
   if (amountValue.value <= 0) {
     document.getElementById("wrongValue");
-    return (wrongValue.innerHTML = "Wartośc musi być większa niż zero");
+    wrongValue.textContent = "Wartość musi być większa niż zero";
+    return;
   }
 
   wrongValue.innerHTML = "";
-  wrongCurrency.innerHTML = "";
 
-  fetch(`http://api.nbp.pl/api/exchangerates/rates/A/${chooseCurrency.value}`)
+  fetch(`https://api.nbp.pl/api/exchangerates/rates/A/${chooseCurrency.value}`)
     .then((response) => response.json())
     .then((data) => {
       const rates = data.rates?.[0]?.mid;
